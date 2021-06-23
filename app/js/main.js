96f999dec5c84601199ade04b4252c86d2224f23
Tatiana <snow_white_girl@bk.ru>
2021-06-23 11:20:17 +0300

    html and styles for Blog page are ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 $(function () {
-  /*    $('.header__btn').on('click', function {
-      $('.rightside-menu').removeClass('rightside-menu--close');
+  $('.header__btn').on('click', function () {
+    $('.rightside-menu').removeClass('rightside-menu--close');
   });
-  $('.rightside-menu__btn-close').on('click', function {
-      $('.rightside-menu').addClass('rightside-menu--close');
-  }); */
+  $('.rightside-menu__btn-close').on('click', function () {
+    $('.rightside-menu').addClass('rightside-menu--close');
+  });
   $('.top__slider').slick({
     dots: true,
     arrows: false,
@@ -16,6 +16,13 @@ $(function () {
     slidesToScroll: 3,
     dots: true,
     arrows: false,
+  });
+
+  $('.article-slider__box').slick({
+    prevArrow: `<button type="button" class="article-slider__arrow article-slider__arrow--left"><img  class="article-slider__icon-arrow" src="images/icon-slide-back.svg" alt="arrow icon"></button>`,
+    nextArrow: `<button type="button" class="article-slider__arrow article-slider__arrow--right"><img  class="article-slider__icon-arrow" src="images/icon-slide-next.svg" alt="arrow icon"></button>`,
+    dots: false,
+    arrows: true,
   });
 
   var mixer = mixitup('.gallery__inner', {
